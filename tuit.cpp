e172783846094696f4c8508bfe7cf67df12ef29f
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-11-09 23:48:14 +0500

    done

---
+++ tuit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int num1 = 10; 
+    int num2 = 20;
+
+    float num3 = 3.532222222;
+
+    cout<<"Nun1: "<<num1;
+    cout<<"\nNum2: "<<num2;
+    cout<<"\nNum3: "<<num3;
+
+    return 0;
+}
