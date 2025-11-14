7f15efb3cb27e6fe6f6158777471b263714ae58a
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-11-14 22:29:47 +0500

    zain

--- tuit5.cpp
+++ tuit5.cpp
@@ -3,16 +3,22 @@ using namespace std;
 
 int main(){
 
-    int num1 , num2;
+   int num1 = 40; 
+   int num2 = 50;
 
-    cout<<"Enter the num1:\n";
-    cin>>num1;
+//    cout<<"the value of num1"<<endl;
+//    cin>>num1;
 
-    cout<<"Enter the num2:\n";
-    cin>>num2;
+//    cout<<"the value of num2"<<endl;
+//    cin>>num2;
 
-    cout<<"Sum: "<<num1 % num2;
+    int sum = num1 + num2;
+
+
+   cout<<"Sum is:"<<sum;
+
+   
 
     return 0;
-}
+} 
 
