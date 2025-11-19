3a0d07391edaed917ab3edfcd2ab85504a04a621
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-11-19 23:18:58 +0500

    added

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+int a, b,c;
+    cout<<"enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"enter the value of a:"<<endl;  
+    cin>>b;
+    c= a + b;
+    cout<<"the sum is "<<c<<endl;
+    cout<<"the global c is  "<<::c;
---
+++ tuit7.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int c = 45;
+
+int main(){
+
+    // int a, b,c;
+    // cout<<"enter the value of a:"<<endl;
+    // cin>>a;
+    // cout<<"enter the value of b:"<<endl;  
+    // cin>>b;
+    // c = a + b;
+    // cout<<"the sum is "<<c<<endl;
+    // cout<<a<<endl;
+    // cout<<"the global c is  "<<::c;
+
+float d = 34.4;
+long double e = 34.4;
+cout<<"the value of e is "<<e<<"D is: "<<;
+return 0;
+
+}
